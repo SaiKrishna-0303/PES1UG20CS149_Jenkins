50da0d6ea0acc1d917527de9b842b79b59543f53
SaiKrishna-0303 <119120157+SaiKrishna-0303@users.noreply.github.com>
2023-02-15 01:27:54 -0500

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,5 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "the change is made by PES1UG20CS149\n";
   return 0;
 }
